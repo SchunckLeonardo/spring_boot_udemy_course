084c293fbab260d613d94c1b1846ed00c7cd7e07
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2025-05-09 11:11:36 -0300

    feat: delete and update endpoint

--- src/main/kotlin/com/github/productsapi/controller/ProductController.kt
+++ src/main/kotlin/com/github/productsapi/controller/ProductController.kt
@@ -3,9 +3,11 @@ package com.github.productsapi.controller
 import com.github.productsapi.model.Product
 import com.github.productsapi.repository.ProductRepository
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -35,6 +37,22 @@ class ProductController(
         return productRepository
             .findById(id)
             .orElse(null)
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(
+        @PathVariable id: String
+    ) {
+        productRepository.deleteById(id)
+    }
+
+    @PutMapping("/{id}")
+    fun update(
+        @PathVariable id: String,
+        @RequestBody product: Product
+    ) {
+        product.id = id
+        productRepository.save(product)
     }
 
 }
