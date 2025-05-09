b893a8038541e0b2f8c93e869d297cb89e5c6af7
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2025-05-09 10:56:54 -0300

    feat: getById endpoint

--- src/main/kotlin/com/github/productsapi/controller/ProductController.kt
+++ src/main/kotlin/com/github/productsapi/controller/ProductController.kt
@@ -3,10 +3,13 @@ package com.github.productsapi.controller
 import com.github.productsapi.model.Product
 import com.github.productsapi.repository.ProductRepository
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.Optional
 import java.util.UUID
 
 @RestController
@@ -23,6 +26,15 @@ class ProductController(
         productRepository.save(product)
 
         return product
+    }
+
+    @GetMapping("/{id}")
+    fun getById(
+        @PathVariable id: String
+    ): Product {
+        return productRepository
+            .findById(id)
+            .orElse(null)
     }
 
 }
